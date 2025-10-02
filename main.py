d0546669d904e09abcc70a96b29e63e6276ecb19
Cobat <nathancobat1@gmail.com>
2025-10-02 13:47:32 +0200

    exercice 4

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ def exercice3():
     print("Exercice 3 : Afficher 3 lignes")
     print(" ligne1\n ligne2\n ligne3\n")
 
-
+def exercice4():
+    print("Exercice 4 : Calculer l'âge")
+    annee_de_naissance = int(input("votre année de naissance"))
+    annee_actuelle = 2025
+    age = annee_actuelle - annee_de_naissance
+    print(f"Vous avez environ {age} ans")
 
 
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
-    if choix == "3":
-        exercice3()
+    if choix == "4":
+        exercice4()
     else:
         print("Exercice non reconnu.")
 if __name__ == "__main__":
